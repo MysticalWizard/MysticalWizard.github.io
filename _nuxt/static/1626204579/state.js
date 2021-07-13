b5ff746ae6a8abd46c7f59739602d27fed238b9e
MysticalWizard <MysticalWizard@users.noreply.github.com>
2021-07-13 19:29:41 +0000

    deploy: ab5b6f1870619f3e108b5a7aa713e9debd52237b

---
+++ _nuxt/static/1626204579/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626204579",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"662a6165"}}}}(null,"\u002F"));
